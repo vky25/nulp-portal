d8bb28c7821f147ad6aa5b9ddfc8fa2efe718c5a
vky25 <93203895+vky25@users.noreply.github.com>
2024-06-04 18:39:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,11 +76,11 @@ build_client(){
 build_server(){
     echo "Building server in background"
     echo "copying required files to app_dist"
-    cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
+    cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js webapp ./../../Dockerfile app_dist
     
     # Copy additional files to dist folder
-    echo "copying additional files to dist"
-    cp -r /var/lib/jenkins/elite-ui/nulp-elite-ui/prod-build/webapp /var/lib/jenkins/workspace/Build/Core/Player/src/app/app_dist/dist/webapp
+    #echo "copying additional files to dist"
+    #cp -r /var/lib/jenkins/elite-ui/nulp-elite-ui/prod-build/webapp /var/lib/jenkins/workspace/Build/Core/Player/src/app/app_dist/dist/webapp
     
     
     cd app_dist
